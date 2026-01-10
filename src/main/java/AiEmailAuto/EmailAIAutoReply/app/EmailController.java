6317eedfc8c9c2a00bbd0850d75956fe9a1b8e9d
Sudhanshuy2006 <sudhanshuy213@gmail.com>
2026-01-10 14:35:15 +0530

    Fix CORS using global config

--- src/main/java/AiEmailAuto/EmailAIAutoReply/app/EmailController.java
+++ src/main/java/AiEmailAuto/EmailAIAutoReply/app/EmailController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
-@CrossOrigin(origins = "*")
 public class EmailController {
 
     private final EmailGenerateService emailGenerateService;
 
     @PostMapping("/generate")
-    public ResponseEntity<String> generateEmail(@RequestBody RequestEmail requestEmail){
-        String response = emailGenerateService.generateService(requestEmail);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<String> generateEmail(@RequestBody RequestEmail requestEmail) {
+        return ResponseEntity.ok(
+                emailGenerateService.generateService(requestEmail)
+        );
     }
 }
